184c65322305fb1fc3046c4466fe75d5c02a9fde
luisgerman <27436197+LuisGermanDev@users.noreply.github.com>
2025-03-26 10:49:17 -0400

    add nuevo p

--- app/(stack)/(tabs)/estado/index.tsx
+++ app/(stack)/(tabs)/estado/index.tsx
@@ -8,6 +8,23 @@ const Estado = () => {
     <SafeAreaProvider>
       <SafeAreaView style={[globalStyle.container,{display:'flex',justifyContent:'center'}]}>
         <Text style={globalStyle.h1}>Como estas?</Text>
+        <View style={{display:'flex',flexDirection:'row',gap:10,marginVertical:80}}>
+        <TouchableOpacity>
+            <Text style={{color:'white'}}>Increible</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text >Bien</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text >Meh</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text >Mal</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text >Horrible</Text>
+          </TouchableOpacity>
+        </View>
         <TouchableOpacity style={globalStyle.buttonWellcome}>
           <Text style={globalStyle.buttonTextw}>Guardar</Text>
         </TouchableOpacity>
